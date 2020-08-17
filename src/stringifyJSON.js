217b2fcff1d58b23eed36fd2fdf715a1e7f3f13c
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-17 12:39:24 -0700

    Working tests for all primitive objs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,6 +7,9 @@ var stringifyJSON = function(obj) {
   // your code goes here
   // create return string
   var result = '';
+  if (obj === null) {
+    return 'null';
+  }
   // check what type of obj it is
   // if complex data (array // obj)
   if (Array.isArray(obj)) {
@@ -16,6 +19,13 @@ var stringifyJSON = function(obj) {
     //skip key if it is a function
     //iterate through structure,
   //convert element into strings, concat to result
+  } else if (typeof obj === 'string') {
+  //for primitive data type
+  //if primite, turn object into a string and add to result string
+    result += '"' + obj.toString() + '"';
+  } else {
+    result += obj.toString();
   }
   //if not complex, use toString method
+  return result;
 };
