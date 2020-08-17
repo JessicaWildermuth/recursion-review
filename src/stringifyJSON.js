d04a00ad70154fb46402e06d5c6f558418064ac0
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-17 12:31:17 -0700

    Initial pseudocode planning

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,17 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // create return string
+  var result = '';
+  // check what type of obj it is
+  // if complex data (array // obj)
+  if (Array.isArray(obj)) {
+    //iterate through structure,
+  //convert element into strings, concat to result
+  } else if (typeof obj === 'object') {
+    //skip key if it is a function
+    //iterate through structure,
+  //convert element into strings, concat to result
+  }
+  //if not complex, use toString method
 };
